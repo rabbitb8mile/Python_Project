70c6c0365430426e95c9b1aa9cf4eea26614e3bf
Parth <137251828+rabbitb8mile@users.noreply.github.com>
2023-07-31 19:31:14 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,274 @@
+import random
+import tkinter as tk
+
+
+def weight_gain_program():
+    workout_plan = "Follow this weight gain and muscle gain program:\n\n" \
+                   "Day 1 (Legs):\n" \
+                   "- Squats: 3 sets of 12 reps\n" \
+                   "- Lunges: 3 sets of 12 reps\n" \
+                   "- Leg extensions: 3 sets of 12 reps\n\n" \
+                   "Day 2 (Chest/Arms):\n" \
+                   "- Bench press: 3 sets of 10 reps\n" \
+                   "- Triceps Pushdown: 3 sets of 12 reps\n" \
+                   "- Biceps curl: 3 sets of 12 reps\n\n" \
+                   "Day 3 (Back/Shoulders):\n" \
+                   "- Lat pulldown: 3 sets of 10 reps\n" \
+                   "- Seated back rows: 3 sets of 12 reps\n" \
+                   "- Dumbbell shrugs: 3 sets of 12 reps\n"
+
+    return workout_plan
+
+def weight_gain_program_f():
+    workout_plan = "Follow this weight gain and muscle gain program:\n\n" \
+                   "Day 1 (Legs):\n" \
+                   "- Squats: 3 sets of 10 reps\n" \
+                   "- Leg curling : 3 sets of 10 reps\n" \
+                   "- Glute kickbacks  : 3 sets of 10 reps\n" \
+                   "- Walking: 30 mins at incline level of 2.5 \n\n" \
+                   "Day 2 (Upper Body ):\n" \
+                   "- Bench press: 3 sets of 8 reps\n" \
+                   "- Bent over dumbell rows : 3 sets of 8 reps\n" \
+                   "- Deadlift : 3 sets of 8 reps\n" \
+                   "- Triceps dips seated : 3 sets of 8 reps\n" \
+                   "- Biceps curl: 3 sets of 12 reps\n\n" \
+                   "Day 3 (Full Body / Cardio ):\n" \
+                   "- Jumping jacks : 3 sets of 10 reps\n" \
+                   "- Standing high knees: 3 sets of 10 reps\n" \
+                   "- Burpees: 3 sets of 10 reps\n"
+    
+    return workout_plan
+    
+
+def advance_workout_program():
+    workout_plan = "Follow this advanced workout program :\n\n"\
+    "Day 1 (Legs):\n" \
+                   "- Squats: 5 sets of 15 reps\n" \
+                   "- Lunges with weights : 5 sets of 15 reps\n" \
+                   "- Leg extensions: 5 sets of 12 reps\n\n" \
+                   "- Leg Press : 5 sets of 10 reps\n\n"\
+                   "- Seating Calf Raises : 5 sets of 15 \n\n"\
+     "Day 2 (Chest/Arms):\n" \
+                   "-  Decline Bench press: 5 sets of 15 reps\n" \
+                   "- Bench press : 5 sets of 15 reps \n\n"\
+                   "- Incline Bench Press : 5 sets of 15 reps \n\n"\
+                   "- Machine Flies : 5 sets of 10 reps\n\n"\
+                   "- Incline Dumbell Press : 5 sets of 12 reps\n\n"\
+                   "- Cable Crossovers : 3 sets of 12 reps \n\n"\
+                   "- dumbell pullovers : 3 sets of 12 reps\n\n"\
+                   "- Weighted dips : 3 sets of 10-15 reps\n\n"\
+                   "-Preacher Curls : 4 sets of 10 reps\n\n"\
+                   "- Triceps Pushdown: 3 sets of 12 reps\n" \
+                   "- Biceps curl: 3 sets of 12 reps\n\n" \
+                   "- Incline bicep curls : 3 sets of 12 reps\n\n"\
+                   "Day 3 (Back/Shoulders):\n" \
+                   "- Lat pulldown: 3 sets of 10 reps\n" \
+                   "- Seated back rows: 3 sets of 12 reps\n" \
+                   "- Dumbbell shrugs: 3 sets of 12 reps\n"
+  
+
+    return workout_plan
+
+def weight_loss_program():
+    workout_plan = "Follow this weight loss and fat loss program:\n\n" \
+                   "Day 1 (Cardio):\n" \
+                   "- Running: 30 minutes\n\n" \
+                   "Day 2 (Legs):\n" \
+                   "- Walking: 30 minutes\n" \
+                   "- Squats: 3 sets of 12 reps\n" \
+                   "- Lunges: 3 sets of 12 reps\n\n" \
+                   "Day 3 (Chest/Arms):\n" \
+                   "- Push ups: 3 sets of 10 reps\n" \
+                   "- Triceps Pushdown: 3 sets of 12 reps\n" \
+                   "- Biceps curl: 3 sets of 12 reps\n"
+    
+                
+
+    return workout_plan 
+
+def weight_loss_program_f():
+    workout_plan = "Follow this weight loss and fat loss program:\n\n" \
+                   "Day 1 (Cardio):\n" \
+                   "- Jumping jacks: 30 minutes\n\n" \
+                   "- jogging on spot : 40 seconds with 20 seconds break : 3 sets \n\n" \
+                   "- Crunches : 3 sets of 8 reps \n\n" \
+                   "- Pushups rotations : 3 sets of 8 reps \n\n" \
+                   "- Touch throughs  : 3 sets of 8 reps \n\n" \
+                   "- Plank : 30 seconds with 20 seconds break : 3 sets  \n\n" \
+                   "- Crunches : 3 sets of 8 reps \n\n" \
+                   "Day 2 (Legs):\n" \
+                  "- Squats: 3 sets of 10 reps\n" \
+                   "- Leg curling : 3 sets of 10 reps\n" \
+                   "- Glute kickbacks  : 3 sets of 10 reps\n" \
+                   "- Walking: 30 mins at incline level of 2.5 \n\n" \
+                   "Day 3 (Upper Body ):\n" \
+                   "- Bench press: 3 sets of 8 reps\n" \
+                   "- Bent over dumbell rows : 3 sets of 8 reps\n" \
+                   "- Deadlift : 3 sets of 8 reps\n" \
+                   "- Triceps dips seated : 3 sets of 8 reps\n" \
+                   "- Biceps curl: 3 sets of 12 reps\n\n" \
+    
+                
+
+    return workout_plan
+
+def generate_workout_plan(workouts):
+    workout_plan = "Your daily workout plan for the next 15 days is:\n\n"
+    random.shuffle(workouts)
+    for i, workout in enumerate(workouts, start=1):
+        if workout in ["Push ups", "Bench press", "Triceps Pushdown", "Biceps curl", "Overhead Tricep with dumbbells",
+                       "Incline Bicep Curls", "Incline Bench press"]:
+            workout_plan += f"Exercise {i} (Chest/Arms): {workout}\n"
+        elif workout in ["Squats", "Lunges", "Leg extensions"]:
+            workout_plan += f"Exercise {i} (Legs): {workout}\n"
+        elif workout in ["Pull ups", "Lat pulldown", "Seated back rows", "Dumbbell shrugs"]:
+            workout_plan += f"Exercise {i} (Back/Shoulders): {workout}\n"
+        else:
+            workout_plan += f"Exercise {i} (Cardio): {workout}\n"
+
+    return workout_plan
+
+def generate_workout():
+    workouts = [
+        "Push ups",
+        "Squats",
+        "Lunges",
+        "Pull ups",
+        "Bench press",
+        "Triceps Pushdown",
+        "Biceps curl",
+        "Overhead Tricep with dumbbells",
+        "Incline Bicep Curls",
+        "Incline Bench press",
+        "Overhead shoulder press",
+        "Deadlifts",
+        "Machine Flys",
+        "Leg extensions",
+        "Lat pulldown",
+        "Seated back rows",
+        "Dumbbell shrugs",
+        "Walking",
+        "Running",
+        "Cycling",
+        "Back Row",
+        "Barbell press",
+        "Cable Crossovers",
+        "Preacher Curls ",
+        "Dumbell pullovers",
+        "Decline bench press",
+        "Inlcine bench press",
+        "Weighted dips",
+        "Machine flies",
+        "Leg curling ",
+        "Glute kickbacks",
+        "Tricep dips seated",
+        "Bent over dumbell rows ",
+        "Jumping jacks ",
+        "Jogging on spot",
+        "Crunches ",
+        "Pushups Rotations ",
+        "Touch Throughs ",
+        "Plank"
+    ]
+    
+    weight = float(weight_entry.get())
+    experience = int(exp_entry.get())
+    gender = gender_entry.get().lower()
+    if gender not in ["male", "female"]:
+        print("Enter a valid gender (male or female)")
+        return 
+    
+    age=int(age_entry.get())
+    if age in range(13,18):
+        print("You are too early to join gym ")
+        exit()
+    else:
+        print("you are welcome ")
+
+    workout_type = ""
+    workout_selection = []
+
+
+    if weight < 60 and gender == 'male' and experience in range(0, 3):
+        workout_type = weight_gain_program()
+        workout_selection = workouts[0:10]
+    elif weight < 60 and gender == 'female' and experience in range(0, 3):
+        workout_type = weight_gain_program_f()
+        workout_selection = workouts[30:34]
+    elif experience > 2:
+        workout_type = advance_workout_program()
+        workout_selection = workouts[19:]
+    elif weight>60 and gender=='male':
+        workout_type = weight_loss_program()
+        workout_selection = workouts[10:19]
+    else:
+        workout_type= weight_loss_program_f()
+        workout_selection = workouts[34:39]
+
+    
+
+    workout_plan = generate_workout_plan(workout_selection)
+    result_text.configure(state='normal')
+    result_text.delete(1.0, tk.END)
+    result_text.insert(tk.END, workout_type + '\n' + workout_plan)
+    result_text.configure(state='disabled')
+
+
+
+
+root = tk.Tk()
+root.title("OurFitness - Gym Trainer")
+
+
+
+# Create input fields for all thing
+name_label = tk.Label(root, text="Name:", background="black",foreground="white")
+name_label.grid(row=0, column=0, sticky='e')
+name_entry = tk.Entry(root)
+name_entry.grid(row=0, column=1)
+
+
+age_label = tk.Label(root, text="Age:", background="black", foreground="white")
+age_label.grid(row=1, column=0, sticky='e')
+age_entry = tk.Entry(root)
+age_entry.grid(row=1, column=1)
+
+
+gender_label = tk.Label(root, text="Gender:", background="black", foreground="white" )
+gender_label.grid(row=2, column=0, sticky='e')
+gender_entry = tk.Entry(root)
+gender_entry.grid(row=2, column=1)
+
+
+weight_label = tk.Label(root, text="Weight:", background="black",  foreground="white")
+weight_label.grid(row=3, column=0, sticky='e')
+weight_entry = tk.Entry(root)
+weight_entry.grid(row=3, column=1)
+
+
+exp_label = tk.Label(root, text="Experience:", background="black",foreground="white")
+exp_label.grid(row=4, column=0, sticky='e')
+exp_entry = tk.Entry(root)
+exp_entry.grid(row=4, column=1)
+
+
+
+
+
+# Create submit button
+submit_button = tk.Button(root, text="Submit", command=generate_workout)
+submit_button.grid(row=5, column=0, columnspan=2)
+
+#Create save button 
+# save_button = tk.Button(root, text="Save Workout", command=save_workout)
+# save_button.grid(row=5, column=1, columnspan=2, pady=5)
+
+
+# Create text area for displaying the workout plan
+result_text = tk.Text(root, height=100, width=150)
+result_text.grid(row=7, column=0, columnspan=2)
+result_text.configure(state='disabled')
+
+
+
+root.mainloop()
